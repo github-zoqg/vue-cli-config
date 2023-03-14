9f58a050fd760857b6fa95ff20526fcfe1622eb6
qiang.zhao <qiang.zhao@vhall.com>
2023-03-14 17:27:18 +0800

    minimist 解析scripts参数

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
+const fs = require('fs')
+
+console.log(process.argv,'process.argv')
+const options = require('minimist')(process.argv.slice(2))
+console.log(options.mode,'options')
+console.log(defineConfig(),'defineConfig')
+
+fs.writeFile('config.txt','defineConfig().toString()',(err)=>{
+  if(err){
+    return console.log(err)
+  }
+  console.log('写入config.txt成功')
+})
+
 module.exports = defineConfig({
   transpileDependencies: true
 })
